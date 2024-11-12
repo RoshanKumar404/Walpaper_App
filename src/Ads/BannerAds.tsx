5605f071e7c679b7f8ca77cf0af37a90fc688537
Roshankumar404 <roshankuar951@gmail.com>
2024-11-12 20:16:49 +0530

    added Banner Ads

---
+++ src/Ads/BannerAds.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+
+const adUnitId = __DEV__ ? TestIds.ADAPTIVE_BANNER : 'ca-app-pub-4394707558760404~9878961308';
+
+export default function Liked() {
+  return (
+    <View style={styles.container}>
+      
+      <BannerAd
+        unitId={adUnitId}
+        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+        requestOptions={{
+          requestNonPersonalizedAdsOnly: true,
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
